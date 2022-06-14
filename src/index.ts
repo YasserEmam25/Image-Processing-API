7d2000bba52dfd7426562c021d9955c8d6c34c23
Yasser Emam <YasserEmam2002@gmail.com>
2022-06-14 02:39:25 +0200

    finished good part in the project I need to check if the image exists or not and then do the rest

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,17 @@ import resizeImage from "./utilites/resizeImage";
 const app = express();
 const port = 3000;
 
-app.get("/api/image", async (req, res) => {
+app.get("/api/image", (req, res) => {
+    // get the paramaters from the get request url
     const imageUrl = req.query.url;
     const width = req.query.width as string;
     const height = req.query.height as string;
 
-    await resizeImage.resizeImage(imageUrl as string, width, height);
+    // check if image is in cache or not
+    // if so, return it to user, else add it to cache then show it to user
+    
 
+    // an object to help in sending image
     const options = {
         root: path.join(__dirname, "../assets/cache"),
         dotfiles: 'deny',
@@ -21,7 +25,8 @@ app.get("/api/image", async (req, res) => {
         }
     }
 
-    res.sendFile(`${imageUrl}_${width}_${height}.jpg`, options, (err) => {
+    // return the image in cache
+    res.sendFile(`${imageUrl}_${width}_${height}.jpg`, options, async (err) => {
         console.log('====================================');
         console.log(err);
         console.log('====================================');
